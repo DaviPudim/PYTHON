0c0e98a2bb1dd12332433cd4ca93911934e9a4d9
Davi_Emanuel <104441775+DaviPudim@users.noreply.github.com>
2023-04-25 02:06:34 -0300

    Add files via upload

---
+++ Programs/Python_4.py
@@ -0,0 +1,3 @@
+print("A pequenina aranha escaloui a tromba d'água.")
+print("")
+print("Caiu a chuva e lavou a aranha.")
---
+++ Programs/Python_5.py
@@ -0,0 +1,3 @@
+print("A pequenina aranha\nsubiu a tromba d'água.")
+print()
+print("Caiu a chuva\ne lavou a aranha.")
