ae6ac3767838003559d2cfe7b2e6326927f66509
Davi_Emanuel <104441775+DaviPudim@users.noreply.github.com>
2023-04-25 12:50:30 -0300

    Add files via upload

---
+++ Programs/Python_13.py
@@ -0,0 +1,13 @@
+print("Meu\nnome\né\nBond.", end=" ")
+print("James Bond.");
+
+print("peixe", "salgadinhos", sep="&");
+
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+
+
+
+
